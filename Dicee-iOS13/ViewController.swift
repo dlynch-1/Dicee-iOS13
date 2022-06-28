6d29725ede0b8d06bb14f1a2212ffd332734fd1c
Dan Lynch <dlynch_1@hotmail.com>
2022-06-27 20:55:44 -0500

    Add IBoutlet and IBactions

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -10,11 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // IBOutlet allows me to reference a UI element
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        diceImageView1.image = UIImage(named: "DiceSix")!
+        // diceImageView1.alpha = 0.5
+        diceImageView2.image = UIImage(named: "DiceTwo")!
     }
 
-
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        diceImageView1.image = UIImage(named: "DiceFour")!
+        diceImageView2.image = UIImage(named: "DiceFour")!
+    }
+    
 }
 
